645ecb05db3b4bdaac168a4d085c4e0e3965430f
_jinal__rathod_ <jinalrathod594@gmail.com>
2025-09-03 21:02:53 +0530

    lc13

---
+++ lc/lc13.js
@@ -0,0 +1,52 @@
+// Javascript Regular Expression
+
+// Regex
+
+{
+    let regex = /^a....s$/
+
+    let result = regex.test('apples')
+    let result1 = regex.test('apple')
+    console.log(result);
+    console.log(result1);
+}
+
+{
+    //  []
+    let regex = /[abc]/
+    let regex1 = /[A-Z]/
+
+    let result = regex.test("apple")
+    let result1 = regex1.test("Apple")
+
+    console.log(result);
+    console.log(result1);
+}
+
+//  JavaScript Regex (Regular Expressions)
+
+// Mobile Number Validation
+
+{
+    function validenumber(number) {
+        let regex = /^[6-9]\d{9}$/;
+        console.log(regex.test(number));
+        console.log(regex.test(number));
+    }
+
+    validenumber("7658435554");
+    validenumber("0005402125");
+}
+
+// Email Validation
+
+{
+    function valideEmail(email) {
+        let regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+        console.log(regex.test(email));
+        console.log(regex.test(email));
+    }
+
+    valideEmail("askx56@gamil.com");
+    valideEmail("mqqsk@jak");
+}
